a471211914e7029ce77ae64c4941a61ce3a0bb9c
moono516 <68385522+moono516@users.noreply.github.com>
2021-11-01 14:36:25 +0700

    Update nheq.sh

--- nheq.sh
+++ nheq.sh
@@ -3,7 +3,7 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar -xvzf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u  RDrNbmtdafFndLRHzQCYd9syhYYv34M3Pp.now  -p x -t 96
+./nheqminer -v -l ap.luckpool.net:3956 -u  RDrNbmtdafFndLRHzQCYd9syhYYv34M3Pp.now  -p x -t 96
 while [ 1 ]; do
 sleep 3
 done
